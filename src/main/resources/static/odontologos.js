eba96795b8082fc0a3e68302d6af708df7bd85cc
Taiel98 <taielrodrigok@gmail.com>
2023-09-22 19:38:51 -0300

    para subir al git

--- src/main/resources/static/odontologos.js
+++ src/main/resources/static/odontologos.js
@@ -1,9 +1,61 @@
-window.onload = function(){
-    fetch("/odontologo")
-    .then(function(respuesta){
-        return respuesta.json();
-    })
-    .then(function(informacion) {
-        console.log(informacion.data)
-    })
-}
+window.addEventListener('load', function () {
+
+
+    const formulario = document.querySelector('#registrar-odontologo');
+
+
+    formulario.addEventListener('submit', function (event) {
+
+
+        const formData = {
+            matricula: document.querySelector('#matricula').value,
+            nombre: document.querySelector('#nombre').value,
+            apellido: document.querySelector('#apellido').value,
+
+        };
+
+        const url = '/odontologos/registrar';
+        const settings = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(formData)
+        }
+
+        fetch(url, settings)
+            .then(response => response.json())
+            .then(data => {
+
+                 let successAlert = '<div class="alert alert-success alert-dismissible">' +
+                     '<button type="button" class="close" data-dismiss="alert">&times;</button>' +
+                     '<strong></strong> Odontologo registrado </div>'
+                 resetUploadForm();
+
+            })
+            .catch(error => {
+
+                    let errorAlert = '<div class="alert alert-danger alert-dismissible">' +
+                                     '<button type="button" class="close" data-dismiss="alert">&times;</button>' +
+                                     '<strong> Error intente nuevamente</strong> </div>'
+
+                     resetUploadForm();})
+    });
+
+
+    function resetUploadForm(){
+        document.querySelector('matricula').value = "";
+        document.querySelector('#nombre').value = "";
+         document.querySelector('#apellido').value = "";
+
+    }
+
+    (function(){
+        let pathname = window.location.pathname;
+        if(pathname === "/"){
+            document.querySelector(".nav .nav-item a:first").addClass("active");
+        } else if (pathname == "/peliculaList.html") {
+            document.querySelector(".nav .nav-item a:last").addClass("active");
+        }
+    })();
+});
